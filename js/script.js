69fa5e7c15780e556cdd55f79fe3efb9f4b3e86f
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-02 16:47:18 +0100

    bgc diverso al click

--- js/script.js
+++ js/script.js
@@ -1,39 +1,22 @@
-// const levels = document.getElementById('levels');
-
-// console.log(levels)
-
-// //Create array of options to be added
-// const array = ['Easy','Hard','Crazy'];
-
-// //Create and append select list
-// const selectList = document.createElement('select');
-// // selectList.setAttribute("id", "mySelect");
-// levels.append(selectList);
-// console.log(levels);
-
-// // //Create and append the options
-// // for (var i = 0; i < array.length; i++) {
-// //     var option = document.createElement("option");
-// //     option.setAttribute("value", array[i]);
-// //     option.text = array[i];
-// //     selectList.appendChild(option);
-// // }
+const container = document.querySelector('.containerMain')
 
 
-const myDiv = document.getElementById("myDiv");
+for (let i = 0; i < 100; i++){
+  const sq  = createSquare(container);
+  sq.addEventListener('click', function(){
+    this.classList.add('clicked')
+  })
 
-//Create array of options to be added
-const array = ["Volvo","Saab","Mercades","Audi"];
+  
 
-//Create and append select list
-const selectList = document.createElement("select");
-selectList.setAttribute("id", "mySelect");
-myDiv.append(selectList);
 
-//Create and append the options
-for (let i = 0; i < array.length; i++) {
-    let option = document.createElement("option");
-    option.setAttribute("value", array[i]);
-    option.text = array[i];
-    selectList.append(option);
 }
+
+function createSquare(target){
+  const sq = document.createElement('div');
+  sq.className='square';
+  target.append(sq)
+  return sq
+}
+
+
